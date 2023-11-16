2e899a65d9e8cbfe43bfa179d6d298b4b4f49df6
Lucas <lucasmoura53@gmail.com>
2023-11-16 19:16:12 -0300

    l

--- set.js
+++ set.js
@@ -63,4 +63,51 @@ module.exports = class Set {
     clear() {
         this.items = [];
     }
+    size(){
+        return Object.keys(this.items).length
+    }
+    sizeLegacy() {
+        let count = 0;
+        for(let key in this.items) { 
+            if(this.items.hasOwnProperty(key)) { 
+                count++; // {4}
+            }
+        }
+        return count;
+    };
+    values(){
+        return Object.values(this.items)
+    }
+    valuesLegacy() {
+        let values = [];
+        for(let key in this.items) {
+            if(this.items.hasOwnProperty(key)) {
+                values.push(key);
+            }
+        }
+        return values;
+    };
+    union(outroSet){
+        const unionSet = new Set();
+        this.values().forEach(value => unionSet.add(value));
+        outroSet.values().forEach(value => unionSet.add(value)); 
+        return unionSet;
+    }
+    intersection(otherSet) {
+        const intersectionSet = new Set(); 
+        const values = this.values();
+        const otherValues = otherSet.values(); 
+        let biggerSet = values;
+        let smallerSet = otherValues;
+        if (otherValues.length - values.length > 0) {
+            biggerSet = otherValues;
+            smallerSet = values;
+        }
+        smallerSet.forEach(value => { 
+            if (biggerSet.includes(value)) {
+                intersectionSet.add(value);
+            }
+        });
+        return intersectionSet;
+    }   
 };
